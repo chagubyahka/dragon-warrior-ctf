89ea55a0cf64bf2d848fff9338457ea60e8befbf
Oskar Chan <oskar.chan@student.howest.be>
2026-03-24 13:32:22 +0100

    better hint in cookie's

--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,6 @@
       "7585dc573642c73b134a44fef52bb56e106c0da2d3d0be86275629ea182143ca"
     );
   }
-  document.cookie = "user_pref=TGludDogQUVTICsgVVRGLTggbW9kZQ==; path=/";
+  document.cookie = "user_pref=SGludDogdXNlIEFFUyArIFVURi04IGFuZCBtb2RlIEVDQg==; path=/";
   console.log("po eet graag cookie's")
 })();
